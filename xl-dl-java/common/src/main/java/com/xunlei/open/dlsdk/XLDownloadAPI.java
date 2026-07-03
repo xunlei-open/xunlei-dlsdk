0686b435dee98db9a3a259704fcc3d587380c6fd
Wenfeng <palwfli@gmail.com>
2026-07-03 17:12:14 +0800

    fix: resolve compiler errors in Java SDK module

--- xl-dl-java/common/src/main/java/com/xunlei/open/dlsdk/XLDownloadAPI.java
+++ xl-dl-java/common/src/main/java/com/xunlei/open/dlsdk/XLDownloadAPI.java
@@ -126,7 +126,7 @@ public class XLDownloadAPI {
 
     private static native int version(StringGetter versionGetter);
 
-    private static native int setDownloadUrlAcceleration(boolean enable);
+    public static native int setDownloadUrlAcceleration(boolean enable);
 
     public static int init(String appId, String appVersion, String cfgPath, boolean saveTasks) {
         InitParam param = new InitParam();
--- xl-dl-java/desktop/src/test/java/com/xunlei/open/dlsdk/XLDownloadAPIIntegrationTest.java
+++ xl-dl-java/desktop/src/test/java/com/xunlei/open/dlsdk/XLDownloadAPIIntegrationTest.java
@@ -79,9 +79,10 @@ public final class XLDownloadAPIIntegrationTest {
             }
 
             int setUrlAccelerationResult = XLDownloadAPI.setDownloadUrlAcceleration(true);
-            System.out.println("Download URL acceleration enabled result: " + setUrlAccelerationResult);
+            require(setUrlAccelerationResult == XLDownloadAPI.ERROR_SUCCESS, "set download url acceleration failed: " + setUrlAccelerationResult);
+            Thread.sleep(1000L);
             setUrlAccelerationResult = XLDownloadAPI.setDownloadUrlAcceleration(false);
-            System.out.println("Download URL acceleration disabled result: " + setUrlAccelerationResult);
+            require(setUrlAccelerationResult == XLDownloadAPI.ERROR_SUCCESS, "set download url acceleration failed: " + setUrlAccelerationResult);
 
             throw new RuntimeException("task " + taskId + " did not finish within " + timeoutSeconds + " seconds");
         } finally {
